2c0fdf876e3096fdf6b3aa6dd5356e2ff58e42b5
Khanlimon29 <flick1@list.ru>
2025-12-15 01:52:07 +0300

    добавил бетаальфаверсию трубы с выбором направления

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Teleporter : MonoBehaviour
+{
+    [Header("Настройки")]
+    public Transform[] teleportTargets; // Возможные точки
+    public string teleportableTag = "Teleportable"; // Тег телепортируемых объектов
+    public Collider teleportZone; // Зона телепортации
+
+    private int selectedTargetIndex = 0;
+
+    private List<GameObject> objectsInZone = new List<GameObject>();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger enter");
+        if (other.CompareTag(teleportableTag))
+        {
+            Debug.Log("Nice tag");
+            if (!objectsInZone.Contains(other.gameObject)) {
+                objectsInZone.Add(other.gameObject);
+                Debug.Log("add");
+            }
+            Debug.Log("trig");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (objectsInZone.Contains(other.gameObject)) { 
+            objectsInZone.Remove(other.gameObject);
+            Debug.Log("Remove");
+        }
+    }
+
+    public void SelectTarget(int index)
+    {
+        if (index >= 0 && index < teleportTargets.Length)
+            selectedTargetIndex = index;
+        else
+            Debug.LogWarning("Индекс точки телепортации вне диапазона");
+    }
+
+    public void TeleportObjects()
+    {
+        Debug.Log($"tp try {objectsInZone}");
+        foreach (var obj in objectsInZone)
+        {
+            obj.transform.position = teleportTargets[selectedTargetIndex].position;
+            Debug.Log("tp");
+        }
+    }
+}
